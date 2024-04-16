3e3a97b91aa2c30271e57acf2654bae08e2b5730
Ayush Singh <ayushhsingh2002@gmail.com>
2024-04-16 16:51:54 +0530

    Add Javadoc comments for Repositories

--- src/main/java/com/hibernate/employeeManagement/Repositories/EmployeeRepository.java
+++ src/main/java/com/hibernate/employeeManagement/Repositories/EmployeeRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hibernate.employeeManagement.Entities.EmployeeEntity;
 
+/**
+ * Repository interface for Employee Entity
+ */
+
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long>
 {
   
--- src/main/java/com/hibernate/employeeManagement/Repositories/ProjectRepository.java
+++ src/main/java/com/hibernate/employeeManagement/Repositories/ProjectRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hibernate.employeeManagement.Entities.ProjectEntity;
 
+/**
+ * Repository Interface for Project Entity
+ */
+
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long>
 {
   
